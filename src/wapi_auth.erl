1d425f5c730c6567a09a1e66f75b5f5ea57831c8
Rustem Shaydullin <ndiezel0@gmail.com>
2023-04-13 04:40:57 +0500

    OPS-282: Fix config

--- src/wapi_auth.erl
+++ src/wapi_auth.erl
@@ -132,7 +132,7 @@ get_ip_address(SwagContext) ->
     end.
 
 get_ip_address_from_request(Request) ->
-    IPAddressHeader = genlib_app:env(capi, ip_address_header, <<"x-forwarded-for">>),
+    IPAddressHeader = genlib_app:env(wapi_lib, ip_address_header, <<"x-forwarded-for">>),
     case Request of
         #{headers := #{IPAddressHeader := IPAddress}} ->
             parse_header_ip_address(IPAddress);
